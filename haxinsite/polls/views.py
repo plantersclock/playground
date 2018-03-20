9baf74567f9b03c7a82f4c39754524ead6c26270
Matthew <mattman861@gmail.com>
2018-03-20 00:30:21 -0400

    more updates. Two failures when running tests though

--- haxinsite/polls/views.py
+++ haxinsite/polls/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
 
 from .models import Question
 
@@ -30,4 +32,21 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        """Return the last five published questions
+        (not including those set to be published in the future)."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    ...
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
